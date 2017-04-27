95c1755eab3d3128151c8edee96efb2ec1d40f11
Dmitry Yankovskii <yankovskiidv@mail.ru>
2017-04-27 13:02:43 +0300

    Some fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,19 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'yad@mail.com',
+      :password             => '',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
